0408e0a42efa0a754464f31337a97b677c4a505e
alonfigue <alonsogfm2000@gmail.com>
2022-04-14 02:25:01 -0400

    Component Btn Share to social Media

--- native_app/src/screens/ChapterPages.js
+++ native_app/src/screens/ChapterPages.js
@@ -1,12 +1,17 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button } from 'react-native'
+import React from 'react';
+import ShareBtn from "../components/ShareBtn";
+
 
 const ChapterPages = () => {
+  
   return (
     <View>
-      <Text>ChapterPages</Text>
+    <Text>ChapterPages</Text> 
+    
+    <ShareBtn/>
     </View>
-  )
-}
-
-export default ChapterPages
+    )
+  }
+  
+  export default ChapterPages
